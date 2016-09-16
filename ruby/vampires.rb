31815c9251776757490bc4713e8f1616db2ec74e
Patrick Harrington <patrickh613@gmail.com>
2016-09-16 11:27:29 -0700

    finshed looping and added last string to program

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,20 +1,18 @@
-=begin
-#At the beginning of your program, ask the user how many employees will be processed, then begin the survey process for the first employee. After you print the result for that employee, start the survey over again, and repeat until all the employees have been processed.
+#In addition to the other questions in the survey, use a loop to ask the employee to name any allergies, one at a time. The employee can type “done” when finished.
 
-(Hmm, doing things over and over again ... that certainly sounds like a loop, doesn't it?)
-=end
+#As long as the allergy is not “sunshine,” continue the loop for as long as is needed. If at any point the employee lists “sunshine” as an allergy, skip directly to the result of “Probably a vampire.”
 #break statment/ keep in mind
-
 
 puts "how many employees will be processed?"
 num_employees = Integer(gets.chomp)
 num_loops = 0
 
 while num_loops < num_employees
-
+  num_loops = num_loops + 1
 
   puts "What is your name?"
   name = gets.chomp
+
 
   puts "how old are you and what year were you born"
   input = gets.chomp
@@ -30,7 +28,9 @@ while num_loops < num_employees
     age_right = false
   end
 
-  puts "Our cafeteria serves garlic bread. Should we get some for you?"
+
+
+  puts "Our cafeteria serves garlic bread. Should we get some for you"
   bread_response = gets.chomp
 
   if bread_response == "yes"
@@ -39,6 +39,8 @@ while num_loops < num_employees
     wants_bread = false
   end
 
+
+
   puts "Would you like to enroll in the company's health insurance?"
   health_insurance_response = gets.chomp
 
@@ -46,39 +48,37 @@ while num_loops < num_employees
     wants_insurance = true
   else
     wants_insurance = false
-
   end
-
-
-
-
 
   # If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
   # If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
   # If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
   # Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
   # Otherwise, print “Results inconclusive.”
-  puts "age_right is " + (age_right ? "true" : "false")
-  puts "wants_bread is " + (wants_bread ? "true" : "false")
-  puts "wants_insurance is " + (wants_insurance ? "true" : "false")
 
+  puts "could you please name any allergies you have?"
+  allergy_response = ""
 
-  if (age_right && (wants_bread || wants_insurance))
-    puts "Probably not a Vampire"
-  elsif (!age_right && (!wants_bread || !wants_insurance))
-    puts "probably a vampire"
-  elsif (!age_right && !wants_bread && !wants_insurance)
-    puts "definiely a vampire"
-  elsif (name == "Drake Cula" || name =="Tu Fang")
-    puts "definiely a vampire"
-  else
-    puts "results inconclusive"
+  until allergy_response == "done"
+    allergy_response = gets.chomp
+    if allergy_response == "sunshine"
+      puts "Probably a Vampire"
+      break
+    end
   end
 
-  num_loops = num_loops + 1
-
-
-
-
+  if allergy_response != "sunshine"
+    if (age_right && (wants_bread || wants_insurance))
+      puts "Probably not a Vampire"
+    elsif (!age_right && !wants_bread && !wants_insurance)
+      puts "definiely a vampire"
+    elsif (!age_right && (!wants_bread || !wants_insurance))
+      puts "probably a vampire"
+    elsif (name == "Drake Cula" || name =="Tu Fang")
+      puts "definiely a vampire"
+    else
+      puts "results inconclusive"
+    end
+  end
 end
-
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
