6be223b00d241619e0c92d870605db54af4ed5f9
Stephen D. Wurster <wursters27@gmail.com>
2016-12-25 01:32:16 -0800

    finished 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+class Puppy
+end
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
+
+
+str = String.new
+str.length 
+str.empty?
+str += "huh."
+greeting = String.new("hello")
+greeting.length
+greeting.upcase
